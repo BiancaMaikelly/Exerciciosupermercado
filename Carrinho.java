a56e120be0ab33190833d2e2e954ef62e6ed1a53
BIANCA MAIKELY SCHADE BAYER <bianca.bayer@alunos.sc.senac.br>
2019-11-05 21:28:22 -0300

    enviando

---
+++ Carrinho.java
@@ -0,0 +1,14 @@
+public class Carrinho{
+  
+
+  Date datadacompra;
+
+
+
+
+
+
+
+
+
+}
---
+++ Mercadovsf.java
@@ -0,0 +1,50 @@
+public class Mercadovsf{
+
+
+public static void main(String[] args){
+
+Cliente cliente1 = new cliente();
+cliente1.nome = "Bia";
+cliente.datadenascimento = "16/01/1992";
+cliente.descoto = 0,25;
+
+Cliente cliente2 = new cliente();
+cliente2.nome = "Cely";
+cliente2.datadenascimento = "26/11/1995";
+cliente2.desconto = 8,70;
+
+Cliente cliente3 = new cliente();
+cliente3.nome = "Carlos";
+cliente3.datadenascimento = "28/07/2001";
+cliente3.desconto = 5,89;
+
+Cliente cliente4 = new cliente();
+cliente4.nome = "Maby";
+cliente4.datadenascimento = "28/05/2002";
+cliente4.desconto = 6,30;
+
+Cliente cliente5 = new
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
